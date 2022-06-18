1b99a97aef9f867b96229cac5b531553340b09c2
RAJ <61533823+M-Raj123@users.noreply.github.com>
2022-06-18 19:33:47 +0530

    Update Bar_Graph_Pattern.java

--- Skillrack/Bar_Graph_Pattern.java
+++ Skillrack/Bar_Graph_Pattern.java
@@ -11,34 +11,38 @@ output:
 
 
 import java.util.*;
-public class Hello {
+public class Hello 
+{
     public static void main(String[] args) 
     {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(),mx=0;
-		int arr[] = new int[n];
-		for(int i=0;i<n;i++)
-		{
-		       arr[i] = sc.nextInt();
-		       if(mx<arr[i]) mx = arr[i];
-		}
-		System.out.println(mx);
-		for(int i=0;i<n;i++) arr[i] = mx - arr[i] ;
-		for(int i=0;i<mx;i++)
-		{
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt(),mx=0;
+	int arr[] = new int[n];
+	for(int i=0;i<n;i++)
+	{
+	       arr[i] = sc.nextInt();
+	       if(mx<arr[i]) mx = arr[i];
+	}
+	
+	System.out.println(mx); // finding row 
+	
+	for(int i=0;i<n;i++) arr[i] = mx - arr[i];
+	    
+	for(int i=0;i<mx;i++)
+	{
 
-		    for(int j=0;j<n;j++)
-		    {
-			if(i>=arr[j])
-			{
-			    System.out.print("*");
-			}
-			else{
-			    System.out.print("-");
-			}
-		    }
-		    System.out.println();
+	    for(int j=0;j<n;j++)
+	    {
+		if(i>=arr[j])
+		{
+		    System.out.print("*");
 		}
+		else{
+		    System.out.print("-");
+		}
+	    }
+	    System.out.println();
+	}
 
-     }
+    }
 }
