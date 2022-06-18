077feae776f6caf4e0a1cb9196fd2f265440d06e
RAJ <61533823+M-Raj123@users.noreply.github.com>
2022-06-18 19:32:14 +0530

    Update Bar_Graph_Pattern.java

--- Skillrack/Bar_Graph_Pattern.java
+++ Skillrack/Bar_Graph_Pattern.java
@@ -9,28 +9,36 @@ output:
 
 */
 
+
 import java.util.*;
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n = sc.nextInt(),mx=0;
 		int arr[] = new int[n];
-		for(int i=0;i<n;i++)  arr[i] = n - sc.nextInt();
 		for(int i=0;i<n;i++)
 		{
-		   
+		       arr[i] = sc.nextInt();
+		       if(mx<arr[i]) mx = arr[i];
+		}
+		System.out.println(mx);
+		for(int i=0;i<n;i++) arr[i] = mx - arr[i] ;
+		for(int i=0;i<mx;i++)
+		{
+
 		    for(int j=0;j<n;j++)
 		    {
-		        if(i==arr[j])
-		        {
-		            System.out.print("*");
-		            arr[j]++;
-		        }
-		        else{
-		            System.out.print("-");
-		        }
+			if(i>=arr[j])
+			{
+			    System.out.print("*");
+			}
+			else{
+			    System.out.print("-");
+			}
 		    }
 		    System.out.println();
 		}
-	}
+
+     }
 }
