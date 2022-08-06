cb03d480d637002d3db1fe2002e3452a1a421df3
RAJ <61533823+M-Raj123@users.noreply.github.com>
2022-08-06 21:27:04 +0530

    Nearest_Palindrome

---
+++ Skillrack/Nearest_Palindrome.java
@@ -0,0 +1,19 @@
+/*
+
+input  : 100   120   1232
+output : 99    121   1221
+
+*/
+import java.util.*;
+class Nearest_Palindrome
+{
+  public static void main(String[] args)
+  {
+        long num = sc.nextInt();
+        long n=num,m=num;
+        while(true)
+        {
+            
+        }
+  }
+} 
