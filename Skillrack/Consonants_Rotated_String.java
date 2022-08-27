1c64a141d8f2c51b470ca61f279080e10ab8cb36
RAJ <61533823+M-Raj123@users.noreply.github.com>
2022-08-27 23:56:33 +0530

    Consonants Rotated String

---
+++ Skillrack/Consonants_Rotated_String.java
@@ -0,0 +1,39 @@
+/*
+Consonants Rotated String in anti-clockwise direction
+
+input:  aabaajaaks 2
+output: aakaasaabj
+
+input:  helloworld 5
+output: ledhololwr 
+*/
+
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String k = sc.next();
+		int n = sc.nextInt();
+		String temp = "";
+		for(int i=0;i<k.length();i++)
+		{
+		    char ch = k.charAt(i);
+		    if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u')
+		    {
+		        temp+=ch;
+		    }
+		}
+		int p = 0;
+		while(p<k.length())
+		{
+		    char ch = k.charAt(p);
+		    if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u')
+		    {
+		        System.out.print(temp.charAt(n++));
+		        if(n==temp.length()) n=0;
+		    }
+		    else System.out.print(k.charAt(p));
+		    p++;
+		}
+	}
+}
