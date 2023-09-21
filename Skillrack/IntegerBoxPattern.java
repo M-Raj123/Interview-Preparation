7bdd46d219f58cc04e625d8d31e36119ea31d61d
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-09-21 22:29:03 +0530

    IntegerBoxPattern

---
+++ Skillrack/IntegerBoxPattern.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) 
+    {
+    		Scanner sc = new Scanner(System.in);
+    		String str = sc.next();
+    		for(int i=0;i<3;i++)
+    		{
+    		    for(int j=0;j<str.length()+2;j++)
+    		    {
+    		        if( (i==0 || i==2) )
+    		        {
+    		            if(j==0 || j==str.length()+1)
+    		            {
+    		                System.out.print("+");
+    		            }
+    		            else System.out.print("-");
+    		        }
+    		        else
+    		        {
+    		            System.out.print("|"+str+"|");
+    		            break;
+    		        }
+    		    }
+    		    System.out.println();
+    		}
+	  }
+}
